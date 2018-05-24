7452611a4d522e4cffa2d129af2cfa35f754f0fc
MAGGI\eduardo.faneli <eduardo.faneli@amaggi.com.br>
2018-05-24 13:38:29 -0400

    Projeto Delphi 7

---
+++ Delphi_7/GerarScript.dpr
@@ -0,0 +1,13 @@
+program GerarScript;
+
+uses
+  Forms,
+  uGerarScript in 'uGerarScript.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
